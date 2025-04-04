f1644ea1665f7bac435922fad96564da68bdb41b
rocioliceraas <rocioliceras_02@hotmail.com>
2025-04-04 12:08:12 +0200

    working but not using ofxAudioFile

--- example_2_basic_spatialisation_OF/src/ofApp.cpp
+++ example_2_basic_spatialisation_OF/src/ofApp.cpp
@@ -23,9 +23,9 @@ void ofApp::setup(){
 	 std::cin >> iBufferSize;
 	 std::cin.ignore();
 
-	 /// AUDIO setup
+	/// AUDIO setup
 	 AudioSetup();
-
+	 
 	 /// BRT Global Parametert setup
 	 globalParameters.SetSampleRate(SAMPLERATE); // Setting sample rate
 	 globalParameters.SetBufferSize(iBufferSize); // Setting buffer size
@@ -39,6 +39,91 @@ void ofApp::setup(){
 	 Common::CTransform listenerPosition = Common::CTransform();		 // Setting listener in (0,0,0)
 	 listenerPosition.SetPosition(Common::CVector3(0, 0, 0));
 	 listener->SetListenerTransform(listenerPosition);
+
+
+
+
+	 /////////////////////////////////////
+	 // Create and connnect BRT modules
+	 /////////////////////////////////////
+	 char selection = ShowConfigurationMenu();
+	 if (selection == 'A') {
+		 configurationA.Setup(&brtManager, LISTENER_ID);
+		 configurationA.LoadResources(&brtManager, LISTENER_ID);
+	 } else if (selection == 'B') {
+		 configurationB.Setup(&brtManager, LISTENER_ID);
+		 configurationB.LoadResources(&brtManager, LISTENER_ID);
+		 configurationB.ConfigureFreeFieldEnviromentModel(&brtManager, true, true);
+	 } else if (selection == 'C') {
+		 configurationC.Setup(&brtManager, LISTENER_ID);
+		 configurationC.LoadResources(&brtManager, LISTENER_ID);
+	 } else {
+		 std::cout << "Invalid option. Exiting program.\n";
+		
+	 }
+
+	 /////////////////////
+	 // Create Sources
+	 /////////////////////
+	 source1BRT = CreateSimpleSoundSource("speech");
+	 source2BRT = CreateSimpleSoundSource("steps");
+
+	 if (selection == 'A') {
+		 configurationA.ConnectSoundSource(&brtManager, "speech");
+		 configurationA.ConnectSoundSource(&brtManager, "steps");
+	 } else if (selection == 'B') {
+		 configurationB.ConnectSoundSource(&brtManager, "speech");
+		 configurationB.ConnectSoundSource(&brtManager, "steps");
+	 } else if (selection == 'C') {
+		 configurationC.ConnectSoundSource(&brtManager, "speech");
+		 configurationC.ConnectSoundSource(&brtManager, "steps");
+	 }
+
+	 /////////////////////
+	 // Setup Sources
+	 /////////////////////
+	 Common::CTransform source1 = Common::CTransform();
+	 source1.SetPosition(Spherical2Cartesians(SOURCE1_INITIAL_AZIMUTH, SOURCE1_INITIAL_ELEVATION, SOURCE1_INITIAL_DISTANCE));
+	 source1BRT->SetSourceTransform(source1);
+
+	 source2Azimuth = SOURCE2_INITIAL_AZIMUTH;
+	 source2Elevation = SOURCE2_INITIAL_ELEVATION;
+	 source2Distance = SOURCE2_INITIAL_DISTANCE;
+	 Common::CTransform source2 = Common::CTransform();
+	 source2.SetPosition(Spherical2Cartesians(source2Azimuth, source2Elevation, source2Distance));
+	 source2BRT->SetSourceTransform(source2); // Set source2 position
+	 showSource2PositionCounter = 0;
+
+	 // /////////////////////
+	 // Load Wav Files
+	 /////////////////////
+	 LoadWav(samplesVectorSource1, SOURCE1_FILEPATH); // Loading .wav file
+	 LoadWav(samplesVectorSource2, SOURCE2_FILEPATH); // Loading .wav file
+
+	  /////////////////////
+	 // Start AUDIO Render
+	 /////////////////////
+	 // Declaration and initialization of stereo buffer
+	 outputBufferStereo.left.resize(iBufferSize);
+	 outputBufferStereo.right.resize(iBufferSize);
+
+	 // Informing user by the console to press any key to end the execution
+	 std::cout << std::endl<< std::endl;
+	 std::cout << "Press any key to start and later press ENTER to finish..." << std::endl;
+	 std::cout << std::endl<< std::endl;
+	 std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Clear input buffer
+	 std::cin.get(); // Wait for a key press
+
+	 // Starting the stream
+	 soundStream.start();
+
+	 // Wait enter to finish
+	 std::cin.ignore();
+	 char temp = getchar();
+
+	 // Stopping and closing the stream
+	 soundStream.stop();
+	 soundStream.close();
 
 }
 
@@ -107,22 +192,173 @@ void ofApp::dragEvent(ofDragInfo dragInfo){
 
 }
 
-/* void ofApp::audioOut(ofSoundBuffer & buffer) {
+
+/// Audio output configuration, using ofxAudioFile
+void ofApp::AudioSetup() {
+
+	// Setting the output parameters
+	ofSoundStreamSettings outputParameters;
+	outputParameters.numOutputChannels = 2;
+
+	//outputParameters.getOutDevice();
+	ofSoundDevice device = ShowSelectAudioDeviceMenu();
+	outputParameters.setOutDevice(device); //Aqui he elegido mis auriculares porque el por defecto no se escuchaba
+
+	// Setting real-time audio output.
+	setRealTimePriority();
+	outputParameters.numBuffers = 4;
+
+	outputParameters.sampleRate = SAMPLERATE;
+	outputParameters.bufferSize = iBufferSize;
+
+	std::cout << "this en setup(): " << this << std::endl;
+	std::cout << "soundStream antes de setOutListener: " << &soundStream << std::endl;
+	outputParameters.setOutListener(this);
+
+	
+
+	try {
+		soundStream.setup(outputParameters);
+		soundStream.stop();
+	} catch (const std::exception & e) {
+		std::cout << "\nERROR: Unable to setup sound stream." << '\n'
+				  << "Exception: " << e.what() << std::endl;
+	}
+}
+
+	/// Function to show the user a menu to choose the audio output device
+ofSoundDevice ofApp::ShowSelectAudioDeviceMenu() {
+
+	auto devices = soundStream.getDeviceList();
+	std::cout << "Number of audio devices found: " << devices.size() << std::endl;
+	int connectedAudioDevices = soundStream.getNumOutputChannels();
+
+	std::cout << std::endl
+			  << std::endl;
+	std::cout << "----------------------------------------" << std::endl;
+	std::cout << "     List of available audio outputs" << std::endl;
+	std::cout << "----------------------------------------" << std::endl
+			  << std::endl;
+
+	for (int i = 0; i < devices.size(); ++i) {
+		std::cout << "Device #" << i << ": " << devices[i].name << std::endl;
+		std::cout << "Output channels: " << devices[i].outputChannels << " ";
+		std::cout << "Input channels: " << devices[i].inputChannels << " ";
+		std::cout << "Default output: " << (devices[i].isDefaultOutput ? "Yes" : "No") << std::endl;
+		std::cout << std::endl;
+	}
+
+	std::cout << std::endl;
+	int selectAudioDevice;
+	do {
+		std::cout << "Please choose which audio output you wish to use: ";
+		std::cin >> selectAudioDevice;
+		std::cin.clear();
+		std::cin.ignore(INT_MAX, '\n');
+	} while (!(selectAudioDevice > -1 && selectAudioDevice <= devices.size()));
+	std::cout << std::endl;
+	return devices[selectAudioDevice];
+}
+
+void ofApp::audioOut(ofSoundBuffer & buffer) {
+	unsigned int uiBufferSize = (unsigned int)iBufferSize;
+	// Setting the output buffer as float
+	std::vector<float> & bufferData = buffer.getBuffer();
+
+	// Obtén un puntero al primer elemento del vector
+	float * floatOutputBuffer = &bufferData[0]; // O también bufferData.data()
+
 	//Overflow/underflow
 	if (buffer.getNumFrames() == 0) {
 		std::cout << "Stream underflow detected!" << std::endl;
 	}
 
-	// buffer initialization
 	// Initializes buffer with zeros
-	buffer.clear();
-	
-	audioProcess(buffer, buffer.getNumFrames());
+	outputBufferStereo.left.Fill(uiBufferSize, 0.0f);
+	outputBufferStereo.right.Fill(uiBufferSize, 0.0f);
 
-	// Si tienes fuentes o posiciones que se mueven, actualízalas aquí
+	audioProcess(outputBufferStereo, uiBufferSize);
+
+	// Declaration and initialization of interlaced audio vector for correct stereo output
+	CStereoBuffer<float> iOutput;
+	iOutput.Interlace(outputBufferStereo.left, outputBufferStereo.right);
+
+	// Buffer filling loop
+	for (auto it = iOutput.begin(); it != iOutput.end(); it++) {
+		floatOutputBuffer[0] = *it; // Setting of value in actual buffer position
+		floatOutputBuffer = &floatOutputBuffer[1]; // Updating pointer to next buffer position
+	}
+
+	
 	MoveSource_CircularHorizontalPath();
 	ShowSource2Position();
-}*/
+}
+
+	/// Function to process audio
+void ofApp::audioProcess(Common::CEarPair<CMonoBuffer<float>> & bufferOutput, int uiBufferSize) {
+	// Declaration, initialization and filling mono buffers
+	CMonoBuffer<float> source1Input(uiBufferSize);
+	FillBuffer(source1Input, wavSamplePositionSource1, positionEndFrameSource1, samplesVectorSource1);
+	CMonoBuffer<float> source2Input(uiBufferSize);
+	FillBuffer(source2Input, wavSamplePositionSource2, positionEndFrameSource2, samplesVectorSource2);
+
+	// Declaration of stereo buffer
+	Common::CEarPair<CMonoBuffer<float>> bufferProcessed;
+
+	source1BRT->SetBuffer(source1Input); // Set samples in the sound source
+	source2BRT->SetBuffer(source2Input); // Set samples in the sound source
+	brtManager.ProcessAll(); // Process all
+	listener->GetBuffers(bufferProcessed.left, bufferProcessed.right); // Get out buffers
+
+	bufferOutput.left += bufferProcessed.left;
+	bufferOutput.right += bufferProcessed.right;
+}
+
+/// Function to fill buffer with audio from the wav file
+void ofApp::FillBuffer(CMonoBuffer<float> & output, unsigned int & position, unsigned int & endFrame, std::vector<float> & samplesVector) {
+	position = endFrame + 1; // Set starting point as next sample of the end of last frame
+	if (position >= samplesVector.size()) // If the end of the audio is met, the position variable must return to the beginning
+		position = 0;
+
+	endFrame = position + output.size() - 1; // Set ending point as starting point plus frame size
+	for (int i = 0; i < output.size(); i++) {
+		if ((position + i) < samplesVector.size())
+			output[i] = (samplesVector[position + i]); // Fill with audio
+		else
+			output[i] = 0.0f; // Fill with zeros if the end of the audio is met
+	}
+}
+
+void ofApp::LoadWav(std::vector<float> & samplesVector, const char * stringIn) {
+	struct WavHeader // Local declaration of wav header struct type (more info in http://soundfile.sapp.org/doc/WaveFormat/)
+	{ // We only need the number of samples, so the rest will be unused assuming file is mono, 16-bit depth and 44.1kHz sampling rate
+		char fill[40];
+		uint32_t bytesCount;
+	} wavHeader;
+
+	FILE * wavFile = fopen(stringIn, "rb"); // Opening of the wav file
+	fread(&wavHeader, sizeof(wavHeader), 1, wavFile); // Reading of the 44 bytes of header to get the number of samples of the file
+	fseek(wavFile, sizeof(wavHeader), SEEK_SET); // Moving of the file pointer to the start of the audio samples
+
+	unsigned int samplesCount = wavHeader.bytesCount / 2; // Getting number of samples by dividing number of bytes by 2 because we are reading 16-bit samples
+	int16_t * sample;
+	sample = new int16_t[samplesCount]; // Declaration and initialization of 16-bit signed integer pointer
+	memset(sample, 0, sizeof(int16_t) * samplesCount); // Setting its size
+
+	uint8_t * byteSample;
+	byteSample = new uint8_t[2 * samplesCount]; // Declaration and initialization of 8-bit unsigned integer pointer
+	memset(byteSample, 0, sizeof(uint8_t) * 2 * samplesCount); // Setting its size
+
+	fread(byteSample, 1, 2 * samplesCount, wavFile); // Reading the whole file byte per byte, needed for endian-independent wav parsing
+
+	for (int i = 0; i < samplesCount; i++)
+		sample[i] = int16_t(byteSample[2 * i] | byteSample[2 * i + 1] << 8); // Conversion from two 8-bit unsigned integer to a 16-bit signed integer
+
+	samplesVector.reserve(samplesCount); // Reserving memory for samples vector
+
+	for (int i = 0; i < samplesCount; i++)
+		samplesVector.push_back((float)sample[i] / (float)INT16_MAX); // Converting samples to float to push them in samples vector
+}
 
 
 ///////////////////////
@@ -144,67 +380,87 @@ void ofApp::ShowIntroduction() {
 	std::cout << "============================================\n\n";
 }
 
-/// Audio output configuration, using ofxAudioFile
-void ofApp::AudioSetup() {
 
-	// Setting the output parameters
-	ofSoundStreamSettings outputParameters;
-	outputParameters.numOutputChannels = 2;
 
-	//outputParameters.getOutDevice();
-	ofSoundDevice device = ShowSelectAudioDeviceMenu();
-	outputParameters.setOutDevice(device); //Aqui he elegido mis auriculares porque el por defecto no se escuchaba
+	// Function to display the configuration menu and get the user’s choice
+	char ofApp::ShowConfigurationMenu() {
+		char option;
 
-	// Setting real-time audio output.
-	setRealTimePriority();
-	outputParameters.numBuffers = 4;
-
-	outputParameters.sampleRate = SAMPLERATE; 
-	outputParameters.bufferSize = iBufferSize;
-
-	outputParameters.setOutListener(this);
-	
-
-	try {
-		ofSoundStreamSetup(outputParameters);
-	} catch (const std::exception & e) {
-		std::cout << "\nERROR: Unable to setup sound stream." << '\n'
-				  << "Exception: " << e.what() << std::endl;
-	
-	}
-}
-
-	/// Function to show the user a menu to choose the audio output device
-	ofSoundDevice ofApp::ShowSelectAudioDeviceMenu() {
-
-		auto devices = soundStream.getDeviceList();
-		std::cout << "Number of audio devices found: " << devices.size() << std::endl;
-		int connectedAudioDevices = soundStream.getNumOutputChannels();
-
-		std::cout << std::endl
-				  << std::endl;
-		std::cout << "----------------------------------------" << std::endl;
-		std::cout << "     List of available audio outputs" << std::endl;
-		std::cout << "----------------------------------------" << std::endl
-				  << std::endl;
-
-		for (int i = 0; i < devices.size(); ++i) {
-			std::cout << "Device #" << i << ": " << devices[i].name << std::endl;
-			std::cout << "Output channels: " << devices[i].outputChannels << " ";
-			std::cout << "Input channels: " << devices[i].inputChannels << " "; 
-			std::cout << "Default output: " << (devices[i].isDefaultOutput ? "Yes" : "No") << std::endl;
-			std::cout << std::endl;
-		}
-
-		std::cout << std::endl;
-		int selectAudioDevice;
 		do {
-			std::cout << "Please choose which audio output you wish to use: ";
-			std::cin >> selectAudioDevice;
-			std::cin.clear();
-			std::cin.ignore(INT_MAX, '\n');
-		} while (!(selectAudioDevice > -1 && selectAudioDevice <= devices.size()));
-		std::cout << std::endl;
-		return devices[selectAudioDevice];
+			std::cout << "===== CONFIGURATION MENU =====\n";
+			std::cout << "A) Sources --> Listener HRTF Model (Nearfield + Convolution) --> Listener\n";
+			std::cout << "B) Sources --> Environment Model (FreeField) --> ListenerHRTFModel (Nearfield + Convolution) --> Listener\n";
+			std::cout << "C) Sources|--> Listener HRTF Model (Nearfield + Convolution) --> |Listener\n";
+			std::cout << "          |--> Listener BRIR Model (Ambisonic)               --> |\n";
+			std::cout << "D) No yet implemented\n";
+			std::cout << "Select an option (A-D): ";
+			std::cin >> option;
+
+			// Convert to uppercase to avoid issues with lowercase input
+			option = std::toupper(option);
+
+			if (option < 'A' || option > 'D') {
+				std::cout << "❌ Invalid option. Please try again.\n";
+			}
+		} while (option < 'A' || option > 'D'); // Repeat until a valid option is chosen
+
+		return option;
 	}
 
+	
+	///////////////////////
+	// SOURCE MOVEMENT
+	///////////////////////
+	void ofApp::MoveSource_CircularHorizontalPath() {
+
+		Common::CVector3 newPosition;
+		source2Azimuth += SOURCE2_INITIAL_SPEED;
+		if (source2Azimuth > 2 * M_PI) source2Azimuth = 0;
+		newPosition = Spherical2Cartesians(source2Azimuth, source2Elevation, source2Distance);
+
+		Common::CTransform sourcePosition = source2BRT->GetSourceTransform();
+		sourcePosition.SetPosition(newPosition);
+		source2BRT->SetSourceTransform(sourcePosition);
+	}
+
+
+	Common::CVector3 ofApp::Spherical2Cartesians(float azimuth, float elevation, float radius) {
+
+		float x = radius * cos(azimuth) * cos(elevation);
+		float y = radius * sin(azimuth) * cos(elevation);
+		float z = radius * sin(elevation);
+
+		Common::CVector3 pos = listener->GetListenerTransform().GetPosition();
+
+		pos.x = pos.x + x;
+		pos.y = pos.y + y;
+		pos.z = 0.0f;
+
+		return pos;
+	}
+
+	void ofApp::ShowSource2Position() {
+
+		showSource2PositionCounter++;
+		if (showSource2PositionCounter == 25) {
+			showSource2PositionCounter = 0;
+			std::cout << "Source 2 --> Azimuth (" << rad2deg(source2Azimuth) << "), Elevation (" << rad2deg(source2Elevation) << "), Distance (" << source2Distance << ")." << std::endl;
+		}
+	}
+	float ofApp::rad2deg(float rad) {
+
+		return (rad * 180.0) / M_PI;
+	}
+
+	///////////////////////
+	// BRT SETUP
+	///////////////////////
+	std::shared_ptr<BRTSourceModel::CSourceSimpleModel> ofApp::CreateSimpleSoundSource(std::string _soundSourceID) {
+		brtManager.BeginSetup();
+		std::shared_ptr<BRTSourceModel::CSourceSimpleModel> _brtSoundSource = brtManager.CreateSoundSource<BRTSourceModel::CSourceSimpleModel>(_soundSourceID);
+		brtManager.EndSetup();
+		if (_brtSoundSource == nullptr) {
+			std::cout << "Error creating sound source" << std::endl;
+		}
+		return _brtSoundSource;
+	}
--- example_2_basic_spatialisation_OF/src/ofApp.h
+++ example_2_basic_spatialisation_OF/src/ofApp.h
@@ -1,18 +1,34 @@
 #pragma once
 
+#define LISTENER_ID "listener1"
+
+#define SAMPLERATE 44100
+
+#define SOURCE1_FILEPATH "../resources/speech.wav"
+#define SOURCE2_FILEPATH "../resources/steps.wav"
+#define HRTFRESAMPLINGSTEP 15
+
+#define SOURCE1_INITIAL_AZIMUTH 90
+#define SOURCE1_INITIAL_ELEVATION 0
+#define SOURCE1_INITIAL_DISTANCE 2
+
+#define SOURCE2_INITIAL_AZIMUTH 0
+#define SOURCE2_INITIAL_ELEVATION 0
+#define SOURCE2_INITIAL_DISTANCE 2
+#define SOURCE2_INITIAL_SPEED 0.001 
+
 #include "ofMain.h"
 #include <cstdio>
 #include <cstring>
 #include "ofxBRT.h"
 #include "ofxAudioFile.h"
 #include <BRTLibrary.h>
+#include <ofxAudioFile.h>
 #include "ConfigurationA.hpp"
 #include "ConfigurationB.hpp"
 #include "ConfigurationC.hpp"
 
-#define LISTENER_ID "listener1"
 
-#define SAMPLERATE 44100
 
 
 Common::CGlobalParameters globalParameters; // Class where the global BRT parameters are defined.
@@ -20,11 +36,26 @@ BRTBase::CBRTManager brtManager; // BRT global manager interface
 std::shared_ptr<BRTBase::CListener> listener; // Pointer to listener mode
 ofSoundStream soundStream;
 
+std::shared_ptr<BRTSourceModel::CSourceSimpleModel> source1BRT; // Pointers to each audio source model
+std::shared_ptr<BRTSourceModel::CSourceSimpleModel> source2BRT; // Pointers to each audio source model
 
-/* Common::CEarPair<CMonoBuffer<float>> outputBufferStereo; // Stereo buffer containing processed audio
+CConfigurationA configurationA; // Configuration class for the example A
+CConfigurationB configurationB; // Configuration class for the example B
+CConfigurationC configurationC; // Configuration class for the example C
+
+float source2Azimuth;
+float source2Elevation;
+float source2Distance;
+int showSource2PositionCounter;
+
+Common::CEarPair<CMonoBuffer<float>> outputBufferStereo; // Stereo buffer containing processed audio
 std::vector<float> samplesVectorSource1; // Storages the audio from the wav files
 std::vector<float> samplesVectorSource2; // Storages the audio from the wav files
-*/
+
+unsigned int wavSamplePositionSource1; // Storages, respectively, the starting and ending position of the frame being rendered for each source
+unsigned int positionEndFrameSource1;
+unsigned int wavSamplePositionSource2;
+unsigned int positionEndFrameSource2;
 
 class ofApp : public ofBaseApp {
 
@@ -43,15 +74,22 @@ class ofApp : public ofBaseApp {
 		void mouseExited(int x, int y);
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-
-		//void audioOut(ofSoundBuffer & buffer);	
+		void gotMessage(ofMessage msg);	
 
 		void ShowIntroduction();
 		void AudioSetup();
 		ofSoundDevice ShowSelectAudioDeviceMenu();
 		char ShowConfigurationMenu();
+
+		void audioOut(ofSoundBuffer & buffer);
+		void audioProcess(Common::CEarPair<CMonoBuffer<float>> & bufferOutput, int uiBufferSize);
+		void FillBuffer(CMonoBuffer<float> & output, unsigned int & position, unsigned int & endFrame, std::vector<float> & samplesVector);
+		void LoadWav(std::vector<float> & samplesVector, const char * stringIn);
+		std::shared_ptr<BRTSourceModel::CSourceSimpleModel> CreateSimpleSoundSource(std::string _soundSourceID);
+		void MoveSource_CircularHorizontalPath();
+		Common::CVector3 Spherical2Cartesians(float azimuth, float elevation, float radius);
 		void ShowSource2Position();
+		float rad2deg(float rad);
 		
 
 
